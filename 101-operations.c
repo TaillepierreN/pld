894b4af0ca47085630fddf7bafbdea4595cb42bc
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-14 14:54:47 +0100

    Initial commit

---
+++ 101-operations.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+int division(int a, int b)
+{
+	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
