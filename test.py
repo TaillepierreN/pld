d24454d0fb5d91a67bad706020e32e1cd915ec6c
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-12-15 09:50:51 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import random
 import ctypes
 
-cops = ctypes.CDLL('./101-operations.dll')
+cops = ctypes.CDLL('./101-operations.so')
 a = random.randint(-111, 111)
 b = random.randint(-111, 111)
 print("{} + {} = {}".format(a, b, cops.add(a, b)))
